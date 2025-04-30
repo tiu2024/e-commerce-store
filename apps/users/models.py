bcb05b4aa26d4cfab4c585a3ed8d555f639fa54d
example <you@example.com>
2025-04-30 20:17:00 +0500

    User app setup

---
+++ apps/users/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class EstoreUser(AbstractUser):
+    """
+        Django ni ozini User modelini ustidan
+        yozvoryapmiz, bzda 3ta tipdagi foy-chi
+        boladi: Admin, Store Admin, Client
+    """
+
+    USER_TYPE_CHOICES = (
+        ('ADMIN', 'Admin'),
+        ('STORE_ADMIN', 'Store Admin'),
+        ('CLIENT', 'Client')
+    )
+
+    user_type = models.CharField(max_length=20,
+            choices=USER_TYPE_CHOICES,
+            default='CLIENT')
+    
+    phone_number = models.CharField(max_length=15,
+            blank=True,
+            null=True)
+    address = models.TextField(blank=True, null=True)
