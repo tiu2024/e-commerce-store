6888d4f7ebc974615c8d72c0bb67eb5a7c6d1ee3
example <you@example.com>
2025-05-03 17:41:57 +0500

    login page

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
-# Create your views here.
+
+def login(request):
+
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid:
+            user = form.get_user()
+            login(request, user)
+
+            return HttpResponse(
+                f"Username: f{request.user.username}, User type: f{request.user.user_type}")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "users/login.html", {'form': form})
